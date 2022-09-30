59fed28b386169ab2e1225758c22e2ebbaf32ba4
TonyVM <antonio.valladaresmartinez@gmail.com>
2022-09-29 23:35:50 -0400

    returning data

--- app.js
+++ app.js
@@ -74,9 +74,9 @@ selectExchange.addEventListener('change', () => {
 })
 
 
-function getDataFromAPIAxios(_endpoint, _exchange) {
+function getDataFromAPIWithAxios(_endpoint, _exchange) {
     axios({
         method: 'GET',
         url: _endpoint
-    }).then().catch()
+    }).then(res => res.data).catch()
 }
